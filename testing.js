a47a640f05a497feb60d7de13a7a2bae038a8bdb
Kemal Wu <kemal-wu@KemaldeMacBook-Pro.local>
2024-05-08 12:04:08 +0800

    test init

---
+++ testing.js
@@ -0,0 +1,32 @@
+testInput =[10,8,7,9,1,5]
+
+function QuickSort( unsortedArray ) {
+	var lowerValues = [];
+    var higherValues = [];
+    
+    var temp = [];
+  
+    var pivotKey = 0;
+    var pivotValue = unsortedArray.shift();
+    
+    for (var i = 0; i < unsortedArray.length; i++) {
+      if (parseInt(unsortedArray[i]) <= pivotValue) {
+        lowerValues.push(unsortedArray[i]);
+		console.log("key and pivotV:", pivotKey,pivotValue)
+      } else if (parseInt(unsortedArray[i]) >= pivotValue) {
+          higherValues.push(unsortedArray[i]);     
+      }
+    }
+	// start from index
+	// fetch value in unsortedArray in order
+	// []
+    temp[pivotKey] = pivotValue;
+    let test = lowerValues.concat(higherValues)
+    return higherValues
+    
+    // return lowerValues.concat(higherValues)
+	
+}
+
+
+QuickSort(testInput)
